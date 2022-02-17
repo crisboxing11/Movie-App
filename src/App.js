fd442645a3cca817c190b3bad0c12dc3a6778adc
crisboxing11 <crisboxing11@gmail.com>
2022-02-16 21:37:14 -0800

    Creating Layout

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
 import React from 'react';
-import { Button} from 'antd';
+import { Layout, Menu, Breadcrumb } from "antd";
 import './Style/App.css';
 
+
 function App() {
+
+  const { Header, Content, Footer } = Layout;
+
   return (
-    <div className="App">
-    <Button type="primary">Button</Button>
-  </div>
-  )
+    <div>
+      <Layout className="layout">
+        <Header>
+          <div className="logo" />
+          <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["2"]}>
+            {new Array(15).fill(null).map((_, index) => {
+              const key = index + 1;
+              return <Menu.Item key={key}>{`nav ${key}`}</Menu.Item>;
+            })}
+          </Menu>
+        </Header>
+        <Content style={{ padding: "0 50px" }}>
+          <Breadcrumb style={{ margin: "16px 0" }}>
+            <Breadcrumb.Item>Home</Breadcrumb.Item>
+            <Breadcrumb.Item>List</Breadcrumb.Item>
+            <Breadcrumb.Item>App</Breadcrumb.Item>
+          </Breadcrumb>
+          <div className="site-layout-content">Content</div>
+        </Content>
+        <Footer style={{ textAlign: "center" }}>
+          Ant Design ©2018 Created by Ant UED
+        </Footer>
+      </Layout>
+      , mountNode,
+    </div>
+  );
 }
 
 export default App;
