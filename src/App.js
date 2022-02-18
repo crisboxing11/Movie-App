143bf0bfddbc66ddb2bdee4e03e8b8011aa4feb9
crisboxing11 <crisboxing11@gmail.com>
2022-02-17 17:32:10 -0800

    Organizing Layout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Layout, Menu, Breadcrumb, Carousel} from "antd";
+import { Layout, Menu, Breadcrumb,Carousel } from "antd";
 import './Style/App.css';
 
 
@@ -18,6 +18,20 @@ function App() {
   return (
     <div>
       <Layout className="layout">
+        <Carousel autoplay>
+          <div>
+            <h3 style={contentStyle}>1</h3>
+          </div>
+          <div>
+            <h3 style={contentStyle}>2</h3>
+          </div>
+          <div>
+            <h3 style={contentStyle}>3</h3>
+          </div>
+          <div>
+            <h3 style={contentStyle}>4</h3>
+          </div>
+        </Carousel>
         <Header>
           <div className="logo" />
           <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["2"]}>
@@ -29,22 +43,7 @@ function App() {
         </Header>
         <Content style={{ padding: "0 50px" }}>
           <Breadcrumb style={{ margin: "16px 0" }}></Breadcrumb>
-          <div className="site-layout-content">
-            <Carousel autoplay>
-              <div>
-                <h3 style={contentStyle}>1</h3>
-              </div>
-              <div>
-                <h3 style={contentStyle}>2</h3>
-              </div>
-              <div>
-                <h3 style={contentStyle}>3</h3>
-              </div>
-              <div>
-                <h3 style={contentStyle}>4</h3>
-              </div>
-            </Carousel>
-          </div>
+          <div className="site-layout-content">Content</div>
         </Content>
         <Footer style={{ textAlign: "center" }}>
           Made by Cristian Arroyo Colon
