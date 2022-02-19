be206707f00e20998c8cf0f0353ac134eabd3c6c
crisboxing11 <crisboxing11@gmail.com>
2022-02-19 15:20:25 -0800

    Added API

---
+++ src/hooks/useMovie.js
@@ -0,0 +1,31 @@
+import { useState, useEffetc } from 'react';
+import imdb from '../components/apis/imdb';
+
+const useMovie = (defaultSearchTerm) => {
+    const [searchTerm, setSearchTerm] = useState(defaultSearchTerm);
+    const [movies, setMovies] = useState([]);
+    const [errorMessage, setErrorMessage] = useState('');
+    
+    const handleSearchTermChange = (event) => {
+        setSearchTerm(event.target.value);
+    };
+    
+    const search = async (searchTerm) => {
+        try {
+        const response = await imdb.get('/', {
+            params: {
+            s: searchTerm,
+            },
+        });
+        setMovies(response.data.Search);
+        } catch (error) {
+        setErrorMessage('Something went wrong');
+        }
+    };
+    
+    useEffetc(() => {
+        search(searchTerm);
+    }, [searchTerm]);
+    
+    return [searchTerm, handleSearchTermChange, movies, errorMessage];
+}
