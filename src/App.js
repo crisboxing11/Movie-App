4b3191e7c91f7abfbd57703be490017814da38cf
crisboxing11 <crisboxing11@gmail.com>
2022-02-19 12:55:47 -0800

    structuring home

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import { Container,Card, CssBaseline, Typography,Box,Button,Grid} from '@mui/material'
+import Landing from './pages/Landing'
+import Login from './pages/Login'   // <-- import Login
+import { Link, Route, Routes } from 'react-router-dom'  //<Route path="/" element={<Home />} /> (Example use after this line)
+//  <Routes>
+//    <Route path="/" element={<Home />} />
+//    <Route path="about" element={<About />} />
+//  </Routes>;
 
 const App = () => {
   return (
     <div>
-      <CssBaseline />
-
-      <Typography variant="h4" component="h1"> Hello World! </Typography>
-      Hello
+      <Typography variant="h1">Buba Chupa</Typography>
     </div>
   );
 }
---
+++ src/pages/Landing.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import {
+  Container,
+  Card,
+  CssBaseline,
+  Typography,
+  Box,
+  Button,
+  Grid,
+} from "@mui/material";
+
+const Landing = () => {
+  return (
+    <div>Landing Pager!!!</div>
+  )
+}
+
+export default Landing
