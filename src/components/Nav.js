39afb8051b6688badb3133f801fe0d4f57f4b332
crisboxing11 <crisboxing11@gmail.com>
2022-02-19 18:14:42 -0800

    Building Nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -17,7 +17,17 @@ const Nav = () => {
   return (
   <Box>
 <AppBar position="static">
+<Toolbar>
+    <IconButton 
+    size="large"
+    edge="start"
+    color="inherit"
+    aria-label="menu"
+    sx={{mr:2}}
+    >
 
+    </IconButton>
+</Toolbar>
 </AppBar>
   </Box>
   )
