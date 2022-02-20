7b9c293cfb2937186ced5bf589ff4b2d9e01aab4
crisboxing11 <crisboxing11@gmail.com>
2022-02-19 18:44:50 -0800

    Building Nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -16,26 +16,26 @@ import MenuIcon from "@mui/icons-material/Menu";
 
 const Nav = () => {
   return (
-  <Box>
-<AppBar className="bar" position="static" sx={{ width: 3/4, mx: 1/2}}>
-<Toolbar>
-    <IconButton 
-    size="large"
-    edge="start"
-    color="inherit"
-    aria-label="menu"
-    sx={{mr:2}}
-    >
-<MenuIcon />
-    </IconButton>
-    <Typography variant="h6" component="div" sx={{ flexGrow: 1}}>
-        Guinea
-    </Typography>
-    <Button color="inherit">Login</Button>
-</Toolbar>
-</AppBar>
-  </Box>
-  )
+    <Box className="bar">
+      <AppBar position="static" sx={{ width: 3 / 4, mx: 1 / 2 }}>
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Guinea
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
 }
 
 export default Nav
