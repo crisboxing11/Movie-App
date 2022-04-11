015e75f96a0bb15a7f5af7035f50d55202802a18
crisboxing11 <crisboxing11@gmail.com>
2022-04-10 17:34:31 -0700

    Starting Auth

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -33,6 +33,14 @@ function Copyright(props) {
 
 const theme = createTheme();
 const Auth = () => {
+    const handleSubmit = (event) => {
+      event.preventDefault();
+      const data = new FormData(event.currentTarget);
+      console.log({
+        email: data.get("email"),
+        password: data.get("password"),
+      });
+    };
   return (
     <div>Auth</div>
   )
